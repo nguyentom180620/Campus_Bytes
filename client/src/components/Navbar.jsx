6b2fa2b6a4682cd63947d9e41875322f56d327d5
tkare <tkareng12336@gmail.com>
2024-11-14 18:08:45 -0500

    fixed button function for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import plate from '../assets/plate.png';
 import search from '../assets/search-icon-white.png';
 
 const Navbar = ({ user, onSignOut }) => {
+  const not_implemented = () => {
+    alert("Not Yet Implemented!");
+  };
   return (
     <div className='navbar w-full flex items-center justify-between bg-[#fff] py-[15px] px-[7%]'>
       <Link to="/">
@@ -13,40 +16,40 @@ const Navbar = ({ user, onSignOut }) => {
       <p className="main_page_title_text headings text-5xl font-custom mt-1">Campus Bytes</p>
 
       <ul className='flex list-none ml-auto mr-5 justify-between '>
-        <li className='font-custom inline-block my-[10px] mx-[20px] text-[18px] cursor-pointer hover:bg-[#272727] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3'>
-          <Link to="/">Home</Link>
+        <li className=''>
+          <Link to="/" className='font-custom inline-block my-[10px] mx-[20px] text-[21px] cursor-pointer hover:bg-[#272727] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3' >Home</Link>
         </li>
         
         {user ? (
           <>
-            <li className='font-custom inline-block my-[10px] mx-[20px] text-[18px] cursor-pointer hover:bg-[#272727] text-[#242424] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3'>
-              <button onClick={onSignOut} className="text-[18px] cursor-pointer bg-transparent border-0  hover:text-white">
+            <li className=''>
+              <button onClick={onSignOut} className="font-custom inline-block my-[10px] mx-[20px] text-[21px] cursor-pointer hover:bg-[#272727] text-[#242424] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3">
                 Logout
               </button>
             </li>
-            <li className='font-semibold inline-block my-[10px] mx-[20px] text-[18px] pt-2.5 '>
+            <li className='font-semibold inline-block my-[10px] mx-[20px] text-[21px] pt-2.5 '>
               Welcome, {user.username}!
             </li>
           </>
         ) : (
           <>
-            <li className='font-custom inline-block my-[10px] mx-[20px] text-[18px] cursor-pointer hover:bg-[#272727] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3'>
-              <Link to="/login">Login</Link>
+            <li className=''>
+              <Link to="/login" className='font-custom inline-block my-[10px] mx-[20px] text-[21px] cursor-pointer hover:bg-[#272727] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3'>Login</Link>
             </li>
-            <li className='font-custom inline-block my-[10px] mx-[20px] text-[18px] cursor-pointer hover:bg-[#272727] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3'>
-              <Link to="/signup">Sign Up</Link>
+            <li className=''>
+              <Link to="/signup" className='font-custom inline-block my-[10px] mx-[20px] text-[21px] cursor-pointer hover:bg-[#272727] hover:text-white rounded-md py-2 px-4 transition-colors hover:scale-110 transition-transform pt-3'>Sign Up</Link>
             </li>
           </>
         )}
       </ul>
 
-      <div className='search-box flex items-center bg-[#242424] py-[5px] px-[20px] rounded-[50px]'>
+      <div className='search-box flex items-center bg-[#333333] py-[5px] px-[20px] rounded-[8px] '>
         <input
-          className='font-custom p-[6px] border-0 outline-0 bg-transparent text-[#fff] text-[18px] max-w-[200px] pt-3'
+          className='font-custom p-[6px] border-0 outline-0 bg-transparent text-[#fff] text-[16px] max-w-[200px] pt-3'
           type='text'
           placeholder='Search...'
         />
-        <img className='w-[30px] cursor-pointer' src={search} alt="search icon" />
+        <img className='w-[30px] cursor-pointer' src={search} alt="search icon" onClick={not_implemented}/>
       </div>
     </div>
   );
